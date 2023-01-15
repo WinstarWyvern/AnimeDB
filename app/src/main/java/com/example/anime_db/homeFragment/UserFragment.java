c42efc297eec70ca1d2c3e7524fbf733d2dc35de
kitsin30 <sinkit123@gmail.com>
2023-01-15 18:34:45 +0700

    commit user profile 3

--- app/src/main/java/com/example/anime_db/homeFragment/UserFragment.java
+++ app/src/main/java/com/example/anime_db/homeFragment/UserFragment.java
@@ -8,14 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 import com.example.anime_db.R;
+import com.example.anime_db.adapter.UserAdapter;
 import com.example.anime_db.model.user.DataUser;
-import com.example.anime_db.model.user.Jpg;
 import com.example.anime_db.model.user.ResponseUser;
 import com.example.anime_db.rest.ApiClient;
 import com.example.anime_db.rest.ApiInterface;
@@ -23,7 +19,6 @@ import com.example.anime_db.rest.ApiInterface;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
 
 public class UserFragment extends Fragment {
@@ -34,74 +29,51 @@ public class UserFragment extends Fragment {
     }
 
     private void setDataUser(DataUser dataUser, View view){
-
-        ImageView imageUser = view.findViewById(R.id.imgUser);
-        TextView userName = view.findViewById(R.id.nameUser);
-        TextView joined = view.findViewById(R.id.joined);
-        TextView lastOnline = view.findViewById(R.id.lastOnlineContent);
-        TextView daysWatch = view.findViewById(R.id.daysWatchContent);
-        TextView meanScore = view.findViewById(R.id.meanScoreContent);
-        TextView watching = view.findViewById(R.id.watchingContent);
-        TextView complete = view.findViewById(R.id.completeContent);
-        TextView onHold = view.findViewById(R.id.onHoldContent);
-        TextView dropped = view.findViewById(R.id.droppedContent);
-        TextView planToWatch = view.findViewById(R.id.planToWatchContent);
-        TextView totalEntries = view.findViewById(R.id.totalEntriesContent);
-        TextView rewatched = view.findViewById(R.id.rewatchedContent);
-        TextView episodeWatch = view.findViewById(R.id.episodeWatchContent);
-
-        Jpg imageUserJpg = new Jpg();
-        imageUserJpg.setImageUrl(dataUser.getImages().getJpg().getImageUrl());
-
-        String userNameStr = dataUser.getUsername();
-        String joinedStr = dataUser.getJoined();
-        String lastOnlineStr = dataUser.getLastOnline();
-        String daysWatchStr = String.valueOf(dataUser.getStatistics().getAnime().getDaysWatched());
-        String meanScoreStr = String.valueOf(dataUser.getStatistics().getAnime().getDaysWatched());
-        String watchingStr = String.valueOf(dataUser.getStatistics().getAnime().getDaysWatched());
-        String completeStr = String.valueOf(dataUser.getStatistics().getAnime().getCompleted());
-        String onHoldStr = String.valueOf(dataUser.getStatistics().getAnime().getOnHold());
-        String droppedStr = String.valueOf(dataUser.getStatistics().getAnime().getDropped());
-        String planToWatchStr = String.valueOf(dataUser.getStatistics().getAnime().getPlanToWatch());
-        String totalEntriesStr = String.valueOf(dataUser.getStatistics().getAnime().getTotalEntries());
-        String rewatchedStr = String.valueOf(dataUser.getStatistics().getAnime().getRewatched());
-        String episodeWatchStr = String.valueOf(dataUser.getStatistics().getAnime().getEpisodesWatched());
-
-        Glide.with(getContext()).load(imageUserJpg.getImageUrl()).into(imageUser);
-        userName.setText(userNameStr);
-        joined.setText(joinedStr);
-        lastOnline.setText(lastOnlineStr);
-        daysWatch.setText(daysWatchStr + " days");
-        meanScore.setText(meanScoreStr);
-        watching.setText(watchingStr + " anime");
-        complete.setText(completeStr + " anime");
-        onHold.setText(onHoldStr + " anime");
-        dropped.setText(droppedStr + " anime");
-        planToWatch.setText(planToWatchStr + " anime");
-        totalEntries.setText(totalEntriesStr + " anime");
-        rewatched.setText(rewatchedStr + " episode");
-        episodeWatch.setText(episodeWatchStr + " episode");
+        UserAdapter userAdapter = new UserAdapter(getContext(), dataUser, view);
+        userAdapter.showImageUser();
+        userAdapter.showUserName();
+        userAdapter.showJoined();
+        userAdapter.showLastOnline();
+        userAdapter.showDaysWatch();
+        userAdapter.showMeanScore();
+        userAdapter.showWatching();
+        userAdapter.showComplete();
+        userAdapter.showOnHold();
+        userAdapter.showDropped();
+        userAdapter.showPlanToWatch();
+        userAdapter.showTotalEntries();
+        userAdapter.showRewatched();
+        userAdapter.showEpisodeWatch();
     }
 
     private void callRetrofit(String userNameData, View view){
         RelativeLayout displayProfile = view.findViewById(R.id.layoutProfile);
         RelativeLayout displayNoUserFound = view.findViewById(R.id.layoutNoUserFound);
+        RelativeLayout displayLoading = view.findViewById(R.id.layoutLoading);
 
         ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
         Call<ResponseUser> call = apiInterface.getUserProfile(userNameData);
         call.enqueue(new Callback<ResponseUser>() {
             @Override
             public void onResponse(Call<ResponseUser> call, Response<ResponseUser> response) {
-                DataUser dataUser = response.body().getData();
-                displayProfile.setVisibility(View.VISIBLE);
-                displayNoUserFound.setVisibility(View.GONE);
-                setDataUser(dataUser, view);
+                if(response.isSuccessful()){
+                    DataUser dataUser = response.body().getData();
+                    displayProfile.setVisibility(View.VISIBLE);
+                    displayNoUserFound.setVisibility(View.GONE);
+                    setDataUser(dataUser, view);
+                    displayLoading.setVisibility(View.GONE);
+                } else{
+                    displayProfile.setVisibility(View.GONE);
+                    displayNoUserFound.setVisibility(View.VISIBLE);
+                    displayLoading.setVisibility(View.GONE);
+                }
             }
 
             @Override
             public void onFailure(Call<ResponseUser> call, Throwable t) {
                 displayProfile.setVisibility(View.GONE);
                 displayNoUserFound.setVisibility(View.VISIBLE);
+                displayLoading.setVisibility(View.GONE);
             }
         });
     }
