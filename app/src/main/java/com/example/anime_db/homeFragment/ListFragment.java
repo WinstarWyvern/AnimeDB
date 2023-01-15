7550b538b0c1957d6e23b478f5193da96fa01eeb
micha <michaelimaran@gmail.com>
2023-01-15 17:17:55 +0700

    Edited Season, Detail Anime

--- app/src/main/java/com/example/anime_db/homeFragment/ListFragment.java
+++ app/src/main/java/com/example/anime_db/homeFragment/ListFragment.java
@@ -1,3 +1,273 @@
+//package com.example.anime_db.homeFragment;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.fragment.app.Fragment;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.os.PersistableBundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.SearchView;
+//
+//import com.example.anime_db.R;
+//import com.example.anime_db.adapter.AnimeAdapter;
+//import com.example.anime_db.model.anime.Datum;
+//import com.example.anime_db.rest.ApiClient;
+//import com.example.anime_db.rest.ApiInterface;
+//import com.example.anime_db.model.anime.Response;
+//
+//import java.util.List;
+//
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//
+//
+//public class ListFragment extends AppCompatActivity {
+//    private AnimeAdapter animeAdapter;
+//    private SearchView searchview;
+//
+//    RecyclerView recyclerView;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_list);
+//
+//        recyclerView = findViewById(R.id.rvAnime);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        CallRetrofit();
+//    }
+//
+//    private void CallRetrofit() {
+//        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+//        Call<Response> call = apiInterface.getAnime();
+//        call.enqueue(new Callback<Response>() {
+//            @Override
+//            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+//                List<Datum> datumList = response.body().getData();
+//                animeAdapter = new AnimeAdapter(ListFragment.this, datumList);
+//                recyclerView.setAdapter(animeAdapter);
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Response> call, Throwable t) {
+//
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_list, container, false);
+//
+//                searchview = (SearchView) view.findViewById(R.id.searchView);
+//        searchview.clearFocus();
+//        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String textSearch) {
+//                if (textSearch.length() > 1){
+//                    ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+//                    Call<Response> call = apiInterface.getSearch(textSearch);
+//                    call.enqueue(new Callback<Response>() {
+//                        @Override
+//                        public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+//                            List<Datum> datumList = response.body().getData();
+//                            animeAdapter = new AnimeAdapter(getContext(), datumList);
+//                            recyclerView.setAdapter(animeAdapter);
+//                        }
+//
+//                        @Override
+//                        public void onFailure(Call<Response> call, Throwable t) {
+//
+//                        }
+//                    });
+//                }
+//                return true;
+//            }
+//        });
+//
+//
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+//
+//        CallRetrofit();
+//
+//        return view;
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.example.anime_db.homeFragment;
+//
+//import android.os.Bundle;
+//
+//import androidx.fragment.app.Fragment;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.SearchView;
+//
+//import com.example.anime_db.R;
+//import com.example.anime_db.adapter.AnimeAdapter;
+//import com.example.anime_db.model.anime.Datum;
+//import com.example.anime_db.rest.ApiClient;
+//import com.example.anime_db.rest.ApiInterface;
+//import com.example.anime_db.model.anime.Response;
+//
+//import java.util.List;
+//
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//
+//
+//public class ListFragment extends Fragment {
+//    private AnimeAdapter animeAdapter;
+//    private SearchView searchview;
+//
+//    RecyclerView recyclerView;
+//
+//    private void CallRetrofit() {
+//        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+//        Call<Response> call = apiInterface.getAnime();
+//        call.enqueue(new Callback<Response>() {
+//            @Override
+//            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+//                List<Datum> datumList = response.body().getData();
+//                animeAdapter = new AnimeAdapter(getContext(), datumList);
+//                recyclerView.setAdapter(animeAdapter);
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Response> call, Throwable t) {
+//
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_list, container, false);
+//
+////                searchview = (SearchView) view.findViewById(R.id.searchView);
+////        searchview.clearFocus();
+////        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+////            @Override
+////            public boolean onQueryTextSubmit(String s) {
+////                return false;
+////            }
+////
+////            @Override
+////            public boolean onQueryTextChange(String textSearch) {
+////                if (textSearch.length() > 1){
+////                    ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+////                    Call<Response> call = apiInterface.getSearch(textSearch);
+////                    call.enqueue(new Callback<Response>() {
+////                        @Override
+////                        public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+////                            List<Datum> datumList = response.body().getData();
+////                            animeAdapter = new AnimeAdapter(getContext(), datumList);
+////                            recyclerView.setAdapter(animeAdapter);
+////                        }
+////
+////                        @Override
+////                        public void onFailure(Call<Response> call, Throwable t) {
+////
+////                        }
+////                    });
+////                }
+////                return true;
+////            }
+////        });
+//
+//        recyclerView = view.findViewById(R.id.rvAnime);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+//
+//        CallRetrofit();
+//
+//        return view;
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package com.example.anime_db.homeFragment;
 
 import android.os.Bundle;
@@ -9,14 +279,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.anime_db.R;
+
 import com.example.anime_db.adapter.AnimeAdapter;
 import com.example.anime_db.model.anime.Datum;
+import com.example.anime_db.model.anime.Response;
 import com.example.anime_db.rest.ApiClient;
 import com.example.anime_db.rest.ApiInterface;
-import com.example.anime_db.model.anime.Response;
 
 import java.util.List;
 
@@ -26,13 +301,17 @@ import retrofit2.Callback;
 
 public class ListFragment extends Fragment {
     private AnimeAdapter animeAdapter;
-    private SearchView searchView;
+    private SearchView searchview;
+    Spinner spinnerSeason, spinnerYear;
+    String season = "winter";
+    String year = "2022";
+
 
     RecyclerView recyclerView;
 
-    private void CallRetrofit() {
+    private void CallRetrofit(){
         ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<Response> call = apiInterface.getAnime();
+        Call<Response> call = apiInterface.getAnimeSeasons(year, season);
         call.enqueue(new Callback<Response>() {
             @Override
             public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
@@ -48,6 +327,8 @@ public class ListFragment extends Fragment {
         });
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -55,11 +336,87 @@ public class ListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
+        spinnerSeason = view.findViewById(R.id.spinner_season);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(
+                this.getContext(),
+                R.array.seasons,
+                R.layout.spinner_season_text
+        );
+        arrayAdapter.setDropDownViewResource(R.layout.spinner_season);
+        spinnerSeason.setAdapter(arrayAdapter);
+        spinnerSeason.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                season = adapterView.getSelectedItem().toString();
+                CallRetrofit();
+                Toast.makeText(getContext(),"This is " + season +"'s Schedule", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        spinnerYear = view.findViewById(R.id.spinner_year);
+        ArrayAdapter<CharSequence> arrayAdapter2 = ArrayAdapter.createFromResource(
+                this.getContext(),
+                R.array.year,
+                R.layout.spinner_year_text
+        );
+        arrayAdapter2.setDropDownViewResource(R.layout.spinner_year);
+        spinnerYear.setAdapter(arrayAdapter2);
+
+        spinnerYear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                year = adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         recyclerView = view.findViewById(R.id.rvAnime);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+//        searchview = (SearchView) view.findViewById(R.id.searchView);
+//        searchview.clearFocus();
+//        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String textSearch) {
+//                if (textSearch.length() > 1){
+//                    ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+//                    Call<Response> call = apiInterface.getSearch(textSearch);
+//                    call.enqueue(new Callback<Response>() {
+//                        @Override
+//                        public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+//                            List<Datum> datumList = response.body().getData();
+//                            animeAdapter = new AnimeAdapter(getContext(), datumList);
+//                            recyclerView.setAdapter(animeAdapter);
+//                        }
+//
+//                        @Override
+//                        public void onFailure(Call<Response> call, Throwable t) {
+//
+//                        }
+//                    });
+//                }
+//                return true;
+//            }
+//        });
 
         CallRetrofit();
 
         return view;
     }
-}
+
+
+        }
--- app/src/main/java/com/example/anime_db/rest/ApiInterface.java
+++ app/src/main/java/com/example/anime_db/rest/ApiInterface.java
@@ -6,15 +6,45 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.Url;
 
 public interface ApiInterface {
     @GET("/v4/seasons/now")
     Call<Response> getAnime(
     );
 
-    @GET("/v4/schedules/{animeName}")
+//    @GET("/v4/seasons")
+//    Call<Response> getSeasons(
+//    );
+
+//    @GET("/v4/seasons/{year}/{season}")
+//    Call<com.example.anime_db.model.season.Response> getAnimeSeasons(
+//            @Path(value="year", encoded = true) String year,
+//            @Path(value="season", encoded = true) String season
+//    );
+
+        @GET("/v4/seasons/{year}/{season}")
+        Call<Response> getAnimeSeasons(
+            @Path(value="year") String year,
+            @Path(value="season") String season
+    );
+
+//    @GET("/v4/seasons/{year}/{season}")
+//    Call<com.example.anime_db.model.season.Response> getAnimeSeasons(
+//            @Url String year,
+//            @Url String season
+//    );
+
+    @GET("/v4/schedules")
     Call<Response> getSchedules(
             @Query("filter") String filter
 //            @Path(value="day", encoded = true) String day
     );
+
+//    @GET("/v4/anime?q={name}&sfw")
+//    Call<Response> getSearch(
+//            @Path("name") String Name
+//    );
+
+
 }
